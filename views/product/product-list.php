ecda28bc27e3b881278595f6a341471dd7fbbfbd
Tran Duy Phat <phattranduy00@gmail.com>
2025-03-08 12:14:00 +0700

    Update product-list.php

--- views/product/product-list.php
+++ views/product/product-list.php
@@ -9,24 +9,43 @@
 
         <!-- CSS: Xóa mũi tên mặc định của Swiper -->
         <style>
+        /* Loại bỏ nội dung pseudo mặc định của Swiper */
         .swiper-button-prev::after,
         .swiper-button-next::after {
             content: none !important;
         }
         </style>
 
-        <!-- Nút điều hướng Swiper (sử dụng Unicode) -->
-        <div class="absolute top-1/2 -translate-y-1/2 left-2 z-10">
-            <div
-                class="swiper-button-prev inline-flex items-center justify-center bg-gray-800 hover:bg-gray-600 text-white rounded-full shadow-lg cursor-pointer p-2 text-2xl">
-                ❮
-            </div>
+        <!-- Nút điều hướng Swiper (prev) -->
+        <div class="absolute top-1/2 -translate-y-1/2 left-32 z-10">
+            <button class="swiper-button-prev inline-flex items-center justify-center
+                 bg-black text-white rounded-full shadow-lg cursor-pointer
+                 p-2 hover:bg-black focus:outline-none">
+                <!-- SVG mũi tên trái (chevron-left) -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" viewBox="0 0 448 512">
+                    <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 
+               12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 
+               288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0 
+               105.4-105.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 
+               0l-160 160z" />
+                </svg>
+            </button>
         </div>
-        <div class="absolute top-1/2 -translate-y-1/2 right-2 z-10">
-            <div
-                class="swiper-button-next inline-flex items-center justify-center bg-gray-800 hover:bg-gray-600 text-white rounded-full shadow-lg cursor-pointer p-2 text-2xl">
-                ❯
-            </div>
+
+        <!-- Nút điều hướng Swiper (next) -->
+        <div class="absolute top-1/2 -translate-y-1/2 right-32 z-10">
+            <button class="swiper-button-next inline-flex items-center justify-center
+                 bg-black text-white rounded-full shadow-lg cursor-pointer
+                 p-2 hover:bg-black focus:outline-none">
+                <!-- SVG mũi tên phải (chevron-right) -->
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 fill-current" viewBox="0 0 448 512">
+                    <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5 
+               -12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 
+               224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0 
+               -105.4 105.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 
+               45.3 0l160-160z" />
+                </svg>
+            </button>
         </div>
 
         <!-- Swiper Slider -->
